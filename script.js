aa0eb7dcdb4721ce03142c49ea9dfde8b372a468
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-02-10 13:49:37 -0500

    Testing script
    testing codepen API and trying to loop into the projects

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var burger = document.querySelector('.navbar-burger');
 var menu = document.querySelector('.navbar-menu');
+var projects = document.querySelector('.parentTile');
+
+var projectUrls = [
+  {
+    title: 'fCC Tribute Page',
+    subtitle: 'subtitle here',
+    userUrl: 'https://codepen.io/jcunanan05/',
+    slug: 'eWgbwB'
+  },
+
+  {
+    title: 'fCC Tribute Page',
+    subtitle: 'subtitle here',
+    userUrl: 'https://codepen.io/jcunanan05',
+    slug: 'eWgbwB'
+  },
+];
 
 
 
@@ -8,3 +25,27 @@ burger.addEventListener('click', toggleBurger);
 function toggleBurger() {
   menu.classList.toggle('is-active');
 }
+
+
+function addProject(project) {
+  projects.innerHTML = 
+  `
+    <section class="tile is-parent">
+      <article class="tile is-parent">
+        <div class="tile is-child">
+          <p class="title">${project.title}</p>
+          <p class="subtitle">${project.subtitle}</p>
+
+          <figure class="is-image is-16by9">
+            <a href="${project.userUrl}/full/${project.slug}" target="blank">
+              <img src="${project.userUrl}/pen/${project.slug}/image/small.png">
+            </a>
+          </figure>
+        </div>
+      </article>
+    </section>
+  `;
+}
+
+addProject(projectUrls[0]);
+addProject(projectUrls[1]);
