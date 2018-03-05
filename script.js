2029d8a588402164208db4176f48209a5dadfb15
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-03-04 23:16:17 -0500

    Experimenting with scroll

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var burger = document.querySelector('.navbar-burger');
 var menu = document.querySelector('.navbar-menu');
+var sections = document.querySelectorAll('section.section');
 
 
-
+//debounce calls the function every 20ms
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+	return function() {
+    var context = this, args = arguments;
+		var later = function() {
+      timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 // event listeners 
 burger.addEventListener('click', toggleBurger);
@@ -12,3 +27,15 @@ function toggleBurger() {
   menu.classList.toggle('is-active');
 }
 
+
+function checkSlide(e) {
+  sections.forEach(section => {
+    //client height is the height of the element
+    //scrollY is the position at the top
+    //innerHeight is the height of the viewport
+    console.log(window.innerHeight);
+  });
+}
+
+
+window.addEventListener('scroll', debounce(checkSlide));
