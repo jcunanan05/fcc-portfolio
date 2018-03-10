221883344ab0b152a4ce42aaed547f63e27950ab
Jonathan Cunanan <j_cunanan05@yahoo.com>
2018-03-10 17:40:31 -0500

    Scroll almost fixed
    Scrollspy almost fixed because of duplicating a line.

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-var burger = document.querySelector('.navbar-burger');
-var menu = document.querySelector('.navbar-menu');
-var sections = document.querySelectorAll('body > section');
-var navbarItems = document.querySelectorAll('.navbar-item');  
-var nav = document.querySelector('nav');
+var burger = document.querySelector(".navbar-burger");
+var menu = document.querySelector(".navbar-menu");
+var sections = document.querySelectorAll("body > section");
+var navbarItems = document.querySelectorAll(".navbar-item");
+var nav = document.querySelector("nav");
 
-
-// event listeners 
-burger.addEventListener('click', toggleBurger);
+// event listeners
+burger.addEventListener("click", toggleBurger);
 
 navbarItems.forEach(navbarItem => {
-  navbarItem.addEventListener('click', hideBurger);
+  navbarItem.addEventListener("click", hideBurger);
 });
 
-
 function toggleBurger() {
-  menu.classList.toggle('is-active');
+  menu.classList.toggle("is-active");
 }
 
 function hideBurger() {
-  if (menu.classList.contains('is-active')) {
-    menu.classList.remove('is-active');
+  if (menu.classList.contains("is-active")) {
+    menu.classList.remove("is-active");
   }
 }
-
 
 //debounce calls the function every xms
 function debounce(func, wait = 19, immediate = true) {
   var timeout;
-	return function() {
-    var context = this, args = arguments;
-		var later = function() {
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
       timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
 function checkSlide(e) {
   var topPosition = window.scrollY;
   var navbarHeight = nav.clientHeight;
+  console.log(`navbarHeight = ${navbarHeight}`);
 
   sections.forEach(section => {
-    var sectionIsNear = (topPosition + navbarHeight*4) >= section.offsetTop;
-    var sectionPassedBy = (topPosition + navbarHeight*4) >= (section.offsetTop + section.clientHeight);
+    var sectionId = "#" + section.getAttribute("id");
+    console.log(`\nsectionId = ${sectionId}`);
+    var sectionIsNear = topPosition + navbarHeight * 4 >= section.offsetTop;
+    console.log(`topPosition = ${topPosition}, section.offsettop = ${section.offsetTop}`);
+    var sectionPassedBy =
+      topPosition + navbarHeight * 4 >=
+      section.offsetTop + section.clientHeight;
+    console.log(`section.clientHeight = ${section.clientHeight}`);
+    console.log(`sectionIsNear = ${sectionIsNear}, sectionPassedBy = ${sectionPassedBy}`);
     //client height is the height of the element
     //window.scrollY is the position at the top
     //window.innerHeight is the height of the viewport
@@ -55,25 +60,26 @@ function checkSlide(e) {
     // console.log('offsetTop: ' + section.offsetTop);
     // console.log('scrollY: ' + topPosition);
 
-    if(sectionIsNear) {
+    var navbarItem = document.querySelector(`a[href="${sectionId}"]`);
+    console.log(`navBarItem = ${navbarItem}`);
+    // deactiviate it here for a test before any activation
+    navbarItem ? navbarItem.classList.remove("is-active") : "";
+
+    if (sectionIsNear) {
       //add is-active class to a navbar-item
-      var sectionId = '#' + section.getAttribute('id');
-      var navbarItem = document.querySelector(`a[href="${sectionId}"]`);
 
       //execute if navbarItem exists
-      navbarItem ? navbarItem.classList.add('is-active') : '';
+      navbarItem ? navbarItem.classList.add("is-active") : "";
     }
 
-    if(sectionPassedBy) {
+    if (sectionPassedBy) {
       //add is-active class to a navbar-item
-      var sectionId = '#' + section.getAttribute('id');
-      var navbarItem = document.querySelector(`a[href="${sectionId}"]`);
 
       //execute if navbarItem exist
-      navbarItem ? navbarItem.classList.remove('is-active') : '';
+      navbarItem ? navbarItem.classList.remove("is-active") : "";
     }
   });
+  console.log("-------------------------------");
 }
 
-
-window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener("scroll", checkSlide);
